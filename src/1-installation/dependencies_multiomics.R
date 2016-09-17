cc32c44b892e11324e0ed2d7328b0d317a83d06d
Hernan Chanfreau <hchanfreau@gmail.com>
2016-09-17 20:01:00 -0300

    multiMiR tar.gz file is copied locally so that the user does not have to manually download and modify variables

--- src/1-installation/dependencies_multiomics.R
+++ src/1-installation/dependencies_multiomics.R
@@ -6,7 +6,9 @@
 
 install.packages("XML")
 install.packages("RCurl")
-install.packages("/home/hernan/Escritorio/multiMiR_1.0.1.tar.gz", repos=NULL, type="source")
+
+multimirFile <- paste(getwd(),"/src/1-installation/dependencies/multiMiR_1.0.1.tar.gz", sep="")
+install.packages(multimirFile, repos=NULL, type="source")
 
 install.packages("heatmap.plus")
 source("http://bioconductor.org/biocLite.R")
