a883206e5a427603857025419f9f7cdab9a90861
Hernan Chanfreau <hchanfreau@gmail.com>
2016-10-22 15:05:16 -0300

    some visual modifications

--- src/4-shiny/ui.R
+++ src/4-shiny/ui.R
@@ -5,13 +5,13 @@ shinyUI(
     sidebarLayout(
       
       sidebarPanel(
-        titlePanel("mRna-miRna analysis"),
-        fileInput("mrnaFile", accept=c("text/csv"), label=h4("mrna File")),
-        fileInput("mirnaFile",accept=c("text/csv"), label=h4("mirna File")),
+        titlePanel("multiOmics"),
+        fileInput("mrnaFile", accept=c("text/csv"), label=h4("mRNA profile")),
+        fileInput("mirnaFile",accept=c("text/csv"), label=h4("miRNA profile")),
         sliderInput("thresholdSlider", label=h4("Threshold"), min=0, max=1, value=0.7, step=0.05),
         # directoryInput('outputDir', label = 'Select a directory for the output', value = '~'),
         # textInput('outputFileName', label = 'Output file name', value = 'inputStep2-matureMirnaXmrna.csv'),
-        actionButton("runCalc", "Run Calculation")
+        actionButton("runCalc", "Run pipeline")
       ),
       mainPanel(
         DT::dataTableOutput('result'),
