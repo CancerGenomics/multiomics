24b3ef1aabcb931894b91598a04449871ef2a8d9
Hernan Chanfreau <hchanfreau@gmail.com>
2018-06-03 21:24:39 -0300

    fix cnv correlation table render with follow up data

--- src/4-shiny/server.R
+++ src/4-shiny/server.R
@@ -421,7 +421,7 @@ shinyServer(function(input, output, session) {
                        progResult <- getPrognosticStatistic(cnvMrnaExpressionData(), number.of.clusters, groupin.FUN=multiomics.cut2, 
                                                             input$cnv.survivalFile$datapath, input$cnv.survival.column.name , 
                                                             input$cnv.event.column.name, minimium.number.of.samples.in.a.group=10)
-                       colnames(sharedValues$cnvMrnaCorrelations) <- c("Gene","Location","CNV-mRNA correlation","p-value", "p_value_fdr_adjusted", "ID")
+                       colnames(sharedValues$cnvMrnaCorrelations) <- c("Gene","Location","CNV-mRNA correlation","p-value", "p_value_fdr_adjusted")
                        # creating a matrix to bind to the actual result        
                        tmp <- matrix(nrow = nrow(sharedValues$cnvMrnaCorrelations), ncol = ncol(progResult)-1 )
                        actual.result <- sharedValues$cnvMrnaCorrelations
@@ -541,54 +541,59 @@ shinyServer(function(input, output, session) {
   })
   
   
-  # output$cnv.correlationSurvival <- renderPlot({
-  #   ERROR.GROUPING="ERROR.GROUPING"
-  #   ERROR.EXECUTING.SURV.FIT.FOR.PLOTTING="ERROR.EXECUTING.SURVFIT.FOR.PLOTTING"
-  #   
-  #   if(!is.null(input$MRNACNVResult_rows_selected)){
-  #     
-  #     selected.gene <- cnvMrnaCorrelations()[input$MRNACNVResult_rows_selected,1]
-  #     selected.gene.row <- which(cnvMrnaExpressionData()==selected.gene)
-  #     expression.vector <- as.numeric(as.vector(cnvMrnaExpressionData()[selected.gene.row,2:ncol(cnvMrnaExpressionData())]))
-  #     
-  #     ######ENGANCHAR CON LA GUI####
-  #     #time<-read.time()
-  #     #event<-read.event()
-  #     #number.of.clusters<-read.number.of.clusters()
-  #     #grouping.FUN<-read.grouping.fun()
-  #     #minimium.number.of.samples.in.a.group<-read.minimium.number.of.samples.in.a.group()
-  #     
-  #     #####ELIMINAR ESTE HARDCODEO UNA VEZ ENGANCHADO########
-  #     minimium.number.of.samples.in.a.group<-1
-  #     event<-c(0,1,0,1,0,1,0,1,0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,1,0,1,0,0,1,1,1,0,0,0,0)
-  #     time<-c(0.00000,22.17659,181.25670,36.43532,105.62630,64.98563,123.53180,46.88296,133.88090,94.48871,121.65910,211.90970,31.29960,66.04920,31.43040,210.89120,27.48600,17.83984,44.84400,46.19302,153.19920,155.95890,181.61810,24.77207,169.52770,112.88710,65.83984,220.09030,32.49281,32.51520,95.14560,49.41360,66.66119,153.16630,158.09450)
-  #     number.of.clusters<-2
-  #     grouping.FUN<-multiomics.cut2
-  #     
-  #     tryCatch({
-  #       #Grouping
-  #       tryCatch(
-  #         {	groups<-grouping.FUN(expression.vector, number.of.clusters)
-  #         #Validation for checking if all groups have got well formed groups
-  #         result.check.groups.are.well.formed<-checkGroupsAreWellFormed(groups, selected.gene, minimium.number.of.samples.in.a.group)	
-  #         if (!result.check.groups.are.well.formed@OK) stop(result.check.groups.are.well.formed@message)
-  #         
-  #         },error=function(e){stop(formatErrorMessage(error.type=ERROR.GROUPING, error.detail=e$message))})
-  #       
-  #       n.groups <- length(unique(groups))
-  #       
-  #       #SurvFit for plotting
-  #       tryCatch({
-  #         surv.fit<-survfit(formula = Surv(time, event) ~ groups)
-  #         #Los colores se asignan as?: el primer color del vector col se asigna al grupo m?s chico, el segundo color al segundo m?s chico y as? siguiendo.
-  #         #Es decir, no importa el orden en el que aparezcan los elementos en el time.Considera solamente el groups. 
-  #         plot(surv.fit,col=c("blue", "red"), xlab="Time", ylab="survival")
-  #         title<-selected.gene
-  #         drawLegends(groups, "Survival", title)
-  #       },error=function(e){stop(formatErrorMessage(error.type=ERROR.EXECUTING.SURV.FIT.FOR.PLOTTING, error.detail=e$message))})
-  #     })
-  #   }
-  # })    
+  output$cnv.ccorrelationSurvival <- renderPlot({
+     ERROR.GROUPING="ERROR.GROUPING"
+     ERROR.EXECUTING.SURV.FIT.FOR.PLOTTING="ERROR.EXECUTING.SURVFIT.FOR.PLOTTING"
+     
+	 if(!is.null(input$cnv.survivalFile) && !is.null(input$MRNACNVResult_rows_selected)){
+       
+	   intersection<-keepSameColumns(cnvMrnaExpressionData(),cnvExpressionData())
+	   mrna.dif.expr<-(intersection[[1]])
+	   cnv.dif.expr<-(intersection[[2]])
+		 
+	   selected.gene <- cnvMrnaCorrelations()[input$MRNACNVResult_rows_selected,1]
+	   selected.gene.row <- which(mrna.dif.expr==selected.gene)
+	   expression.vector <- as.numeric(as.vector(mrna.dif.expr[selected.gene.row,2:ncol(mrna.dif.expr)]))
+	   
+	   
+	   cnv.survival.matrix <- read.table(input$cnv.survivalFile$datapath, header = T)
+	   cnv.survival.matrix<-cnv.survival.matrix[cnv.survival.matrix$X_SAMPLE_ID %in% colnames(cnv.dif.expr),]
+	   survival.name.col <- which(colnames(cnv.survival.matrix)==input$cnv.survival.column.name)
+	   survival.event.col <- which(colnames(cnv.survival.matrix)==input$cnv.event.column.name)
+	   
+	   ######ENGANCHAR CON LA GUI Y ELIMINAR HARCODEO ####
+	   number.of.clusters<-2
+	   grouping.FUN<-multiomics.cut2
+	   minimium.number.of.samples.in.a.group<-1
+	   
+	   time<-as.vector(cnv.survival.matrix[,survival.name.col])
+	   event<-as.vector(cnv.survival.matrix[,survival.event.col])
+	   
+       tryCatch({
+         #Grouping
+         tryCatch(
+           {	groups<-grouping.FUN(expression.vector, number.of.clusters)
+           #Validation for checking if all groups have got well formed groups
+           result.check.groups.are.well.formed<-checkGroupsAreWellFormed(groups, selected.gene, minimium.number.of.samples.in.a.group)	
+           if (!result.check.groups.are.well.formed@OK) stop(result.check.groups.are.well.formed@message)
+           
+           },error=function(e){stop(formatErrorMessage(error.type=ERROR.GROUPING, error.detail=e$message))})
+         
+         n.groups <- length(unique(groups))
+         
+         #SurvFit for plotting
+         tryCatch({
+           surv.fit<-survfit(formula = Surv(time, event) ~ groups)
+           #Los colores se asignan as?: el primer color del vector col se asigna al grupo m?s chico, el segundo color al segundo m?s chico y as? siguiendo.
+           #Es decir, no importa el orden en el que aparezcan los elementos en el time.Considera solamente el groups. 
+           plot(surv.fit,col=c("blue", "red"), xlab="Time", ylab="survival")
+           title<-selected.gene
+           drawLegends(groups, "Survival", title)
+         },error=function(e){stop(formatErrorMessage(error.type=ERROR.EXECUTING.SURV.FIT.FOR.PLOTTING, error.detail=e$message))})
+       })
+     }
+   })    
+  
   
   ###########################################################################
   ########################## METHYLATION - MRNA PIPELINE TAB
