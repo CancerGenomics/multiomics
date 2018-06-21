81996fef637de1669bb395dffa4753515f7e16a5
matias.butti@gmail.com <matias.butti@gmail.com>
2018-06-21 11:10:00 -0300

    Type of correlation: negative or positive

--- src/3-do/API/cnvXmrnas/API_cnv_X_mrnas.R
+++ src/3-do/API/cnvXmrnas/API_cnv_X_mrnas.R
@@ -1,10 +1,10 @@
 #If the CNV has got repeated genes, it will use the first row in the order.
 #It will compare all the genes that will be in both files. That is why the fdr is applied over a vector of size = number of elements in common between both files.
 CnvXMrnas <- function(mrna, cnv, output.path="~/", 
-                                                     output.file.name="cnvXMrna.csv",
-                                                     r.minimium=0.7, 
-                                                     pearsons.method = "pearson", 
-                                                     inc.progress = F){
+                      output.file.name="cnvXMrna.csv",
+                      r.minimium=0.7, 
+                      pearsons.method = "pearson", 
+                      inc.progress = F){
   
   ptm <- proc.time()
   total.rows=nrow(mrna)
@@ -32,7 +32,7 @@ CnvXMrnas <- function(mrna, cnv, output.path="~/",
   res <- matrix(nrow=total.rows,ncol=num.of.result.columns)
   colnames(res)<-(c("Gene","Location", "CNV-mRNA correlation", "p-value", "p_value_fdr_adjusted", "ID"))
   
-
+  
   ids.in.cnv.dataset<-cnv[,1]
   
   print("Start process!")
@@ -42,45 +42,45 @@ CnvXMrnas <- function(mrna, cnv, output.path="~/",
     position.in.cnv.dataset<-which(ids.in.cnv.dataset == actual.gen)
     #Se queda con el primer CNV
     if (length(position.in.cnv.dataset)>0){
-        position.in.cnv.dataset<-position.in.cnv.dataset[1]
-        actual.mrna<-mrna[i,2:ncol(mrna)]
-        actual.cnv<-cnv[position.in.cnv.dataset,2:ncol(cnv)]
-        if ((actual)%%500==0)print(paste("analised ", actual, " from ", total.rows))
-        if ((actual)%%1000==0) {
-          elapsedTime <- (proc.time() - ptm)[3]
-          print(paste(
-            "elapsed time: (seconds)", format2Print(elapsedTime), 
-            " - (minutes)", format2Print(elapsedTime/60), 
-            " - (hours)", format2Print(elapsedTime/60/60)
-          ))
-          remainingTime <- ((total.rows*elapsedTime)/actual) - elapsedTime
-          print(paste("estimated remaining time (seconds)", format2Print(remainingTime),
-                      " - (minutes)", format2Print(remainingTime/60), 
-                      " - (hours)", format2Print(remainingTime/60/60)
-          ))
+      position.in.cnv.dataset<-position.in.cnv.dataset[1]
+      actual.mrna<-mrna[i,2:ncol(mrna)]
+      actual.cnv<-cnv[position.in.cnv.dataset,2:ncol(cnv)]
+      if ((actual)%%500==0)print(paste("analised ", actual, " from ", total.rows))
+      if ((actual)%%1000==0) {
+        elapsedTime <- (proc.time() - ptm)[3]
+        print(paste(
+          "elapsed time: (seconds)", format2Print(elapsedTime), 
+          " - (minutes)", format2Print(elapsedTime/60), 
+          " - (hours)", format2Print(elapsedTime/60/60)
+        ))
+        remainingTime <- ((total.rows*elapsedTime)/actual) - elapsedTime
+        print(paste("estimated remaining time (seconds)", format2Print(remainingTime),
+                    " - (minutes)", format2Print(remainingTime/60), 
+                    " - (hours)", format2Print(remainingTime/60/60)
+        ))
+      }
+      resultado.pearson<-cor.test(as.numeric(actual.mrna),
+                                  as.numeric(actual.cnv), 
+                                  method = pearsons.method)
+      
+      ###MDB: 27/2/2018 - P.ADJUST
+      p.values.all<-append(p.values.all, resultado.pearson$p.value)
+      id<-actual
+      ids<-append(ids, id)
+      
+      if (!is.na(abs(resultado.pearson$estimate))) {
+        if (abs(resultado.pearson$estimate) > r.minimium) {
+          location<-getGeneLocation(actual.gen);
+          newValue<-c(as.character(actual.gen), location,
+                      resultado.pearson$estimate, resultado.pearson$p.value, -9999, id)
+          ###MDB: 26/2/2018 - P.ADJUST
+          res[actual.n.correlated,1:num.of.result.columns] <- newValue
+          actual.n.correlated<-actual.n.correlated+1
+          
+          ###MDB: 26/2/2018 - P.ADJUST
+          p.values.positions.of.correlated.pairs<-append(p.values.positions.of.correlated.pairs, id)
         }
-        resultado.pearson<-cor.test(as.numeric(actual.mrna),
-                                    as.numeric(actual.cnv), 
-                                    method = pearsons.method)
-        
-        ###MDB: 27/2/2018 - P.ADJUST
-        p.values.all<-append(p.values.all, resultado.pearson$p.value)
-        id<-actual
-        ids<-append(ids, id)
-        
-        if (!is.na(abs(resultado.pearson$estimate))) {
-          if (abs(resultado.pearson$estimate) > r.minimium) {
-            location<-getGeneLocation(actual.gen);
-            newValue<-c(as.character(actual.gen), location,
-                        resultado.pearson$estimate, resultado.pearson$p.value, -9999, id)
-            ###MDB: 26/2/2018 - P.ADJUST
-            res[actual.n.correlated,1:num.of.result.columns] <- newValue
-            actual.n.correlated<-actual.n.correlated+1
-            
-            ###MDB: 26/2/2018 - P.ADJUST
-            p.values.positions.of.correlated.pairs<-append(p.values.positions.of.correlated.pairs, id)
-          }
-        }
+      }
     }
     if(inc.progress) {
       incProgress(1/nrow(cnv));
@@ -110,11 +110,11 @@ CnvXMrnas <- function(mrna, cnv, output.path="~/",
 }
 
 CnvXMrnasWCGNA <- function(expression, cnv, output.path="~/", 
-                      output.file.name="cnvXMrna.csv",
-                      r.minimium=0.7, 
-                      pearsons.method = "pearson", 
-                      inc.progress = F){
-
+                           output.file.name="cnvXMrna.csv",
+                           r.minimium=0.7, 
+                           pearsons.method = "pearson", 
+                           inc.progress = F){
+  
   ###MDB: 26/3/2018
   library("WGCNA")
   library("reshape2")
@@ -132,7 +132,7 @@ CnvXMrnasWCGNA <- function(expression, cnv, output.path="~/",
   mirna<-cnv[,2:ncol(cnv)]
   
   # calcultate correlation using wcgna
-  correlation.result <-correlation.with.wcgna(expression, mirna,r.minimium)
+  correlation.result <-correlation.with.wcgna(expression, mirna,r.minimium, keep.pos.cor=T, keep.neg.cor=F)
   colnames(correlation.result)<-(c("Gene","Location", "CNV_mRNA_Correlation", "p-value", "p_value_fdr_adjusted"))
   
   # Apply function to set Gene Location on resulting dataframe. This is specific to this Process
